eab0a597a3888a284b3438bc93002bf6a26beea2
Neil Benolirao <neilskillsets.initiative@gmail.com>
2026-02-05 19:04:50 +0800

    refactor: add tests for addTask.js

--- src/utils/addTask.js
+++ src/utils/addTask.js
@@ -1,1 +1,8 @@
-export const addTask = () => {};
+import { createTask } from "./createTask";
+
+export const addTask = (tasks, title) => {
+  if (!title || !title.trim()) return tasks;
+
+  const newTask = createTask(title);
+  return [...tasks, newTask];
+};
