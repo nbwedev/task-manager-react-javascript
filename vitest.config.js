1b54dd1ef29cb323f6e4d400a81c79fccd67286d
Neil Benolirao <neilskillsets.initiative@gmail.com>
2026-02-06 15:36:38 +0800

    chore(deps-dev): update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true, // Elimanates the need to import (it, describe, expect) every time you write a test
-    setupFiles: "test/setup.js",
+    setupFiles: "test/setup.js", // Runs before each test file
   },
 });
