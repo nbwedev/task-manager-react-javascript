7ee28e8bbbc20ada3f4dfe17353cb600e4d33e33
Neil Benolirao <neilskillsets.initiative@gmail.com>
2026-02-06 15:34:52 +0800

    chore(deps-dev): improve test setup by adding configs, library snippets

---
+++ src/test/setup.js
@@ -0,0 +1,2 @@
+import "@testing-library / jest - dom / vitest";
+// No need to import module everytime you write a test
--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    globals: true, // Elimanates the need to import (it, describe, expect) every time you write a test
+    setupFiles: "test/setup.js",
   },
 });
